923d59e90e102f0c641d300c9224df5abfd31a03
ManoelsonJr <42751783+ManoelsonJr@users.noreply.github.com>
2018-09-08 21:08:46 -0300

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,33 @@
+/* P1.c */
+/* MANOELSON MOREIRA RONDON JUNIOR */
+/* 11811EBI004 */
+#include<stdio.h>
+int main()
+{
+	int estado=0, i=0;
+	char bits[256];
+	printf("Digite o numero a ser verificado: ");
+	scanf("%s", bits);
+	while(bits[i] != '\0')
+	{
+		if(estado== 0 && bits[i] == '0')
+		estado = 0;
+		else if(estado ==0 && bits[i] == '1')
+		estado = 1;
+		else if(estado == 1 && bits[i] == '1')
+		estado = 0;
+		else if(estado == 1 && bits[i] =='0')
+		estado = 2;
+		else if(estado == 2 && bits[i] == '0')
+		estado = 1;
+		else if(estado == 2 && bits[i] == '1')
+		estado = 2;
+		i++;
+	}
+	printf("A sequencia original %s \n ", bits);
+	if(estado==0)
+	printf("Eh multiplo de 3.");
+	else
+	printf("Nao eh multiplo de 3.");
+	return 0;
+}
